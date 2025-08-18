805815c3451b47020e72e5f5824f80159467676a
Saideepthi123 <korupolusaideepthi@gmail.com>
2025-08-17 17:08:53 -0700

    recourse-2

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,11 +1,52 @@
 # Python program for implementation of MergeSort 
+# // Time Complexity : 
+# // Space Complexity : 
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : No
+# merge sort : first divide array into half parts, and recurively sort the each half and then merge the sorted halves into one sorted array
+# since adding the sorted halves into array a new array where the elemsts are sorted  the space complexity is O(n) 
 def mergeSort(arr):
   
   #write your code here
+  if len(arr)<2: # split array into equal parts
+        return arr
+        
+  mid = len(arr)//2
+  
+  left_half = arr[:mid]
+  right_half = arr[mid:]
+  
+  mergeSort(left_half)
+  mergeSort(right_half)
+  
+  i = j = k = 0
+  
+  while i< len(left_half) and j < len(right_half): # sort each array and add the sorted elements into arr
+      if left_half[i] < right_half[j]:
+          arr[k] = left_half[i]
+          i+=1
+      else: 
+          arr[k] = right_half[j]
+          j+=1
+      k += 1
+      
+  while i< len(left_half): # adding rest of the elements in the array
+      arr[k] = left_half[i]
+      i+= 1
+      k += 1
+      
+  while j< len(right_half):
+      arr[k] = right_half[j]
+      j+= 1
+      k+= 1 
+  
+  return arr
+
   
 # Code to print the list 
 def printList(arr): 
-    
+    for i in range(len(arr)):
+        print(arr[i])
     #write your code here
   
 # driver code to test the above code 
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,41 @@
 # Python program for implementation of Quicksort
+# // Time Complexity : 
+# // Space Complexity : 
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : No
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
   #write your code here
+  pivot = arr[h]
+  i = l -1 
+  for j in range(l,h):
+      if arr[j] <= pivot:
+          i+=1
+          arr[i], arr[j] = arr[j] , arr[i] 
+  arr[i+1], arr[h] = arr[h], arr[i+1] 
+
+  return i+1 
 
 
 def quickSortIterative(arr, l, h):
   #write your code here
+  # quick sort can be donw in 2 ways recursive or iterative
+  # iterative approach is 
+  # parition method takes care of putting the pivot in the right place making the array before pivot less than pivot and after the pivot greater than pivot
+  # same way, lets do the parition until left is less than right
+  # use stack to keep track of the left and right pivots 
+  stack = [(l,h)] # intially sent the whole array range left to right 
+  
+  while stack: 
+     l,h = stack.pop() # pops the first index and the last index
+     if l<h: 
+        p = partition(arr,l,h) # we first get the pivot , and we know the left of the pivot are less than the pivot, right > pivot
+        stack.append((l,p-1)) # next is to sort the left array so keep sorting the left array, we know the left arra is sorted once the low index is less than the high index
+        stack.append(p+1,h) # same with right subarray, sort the subarray
+        # finally when both the arrays are sorted there will be no more elements in the stack and the array is sorted 
+
+
+
+
 
